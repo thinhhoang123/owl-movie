de0180f6fe56696805ca5ad335762ad58b4c7d5e
ThinhHoang <atulala4657@gmail.com>
2023-04-15 09:31:04 +0700

    update Content-Security-Policy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,30 @@
 /** @type {import('next').NextConfig} */
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src example.com;
+  style-src 'self' example.com;
+  font-src 'self';  
+`
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  }
+]
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['image.tmdb.org'],
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
   },
 }
 
