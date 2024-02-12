c21b92121ae935ca2429012b5665be1ab2addb80
ThinhHoang <atulala4657@gmail.com>
2024-02-12 21:05:50 +0700

    update code

--- src/components/layout/AppHeader.tsx
+++ src/components/layout/AppHeader.tsx
@@ -50,6 +50,26 @@ export default function AppHeader() {
         'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
     },
   ];
+  const headerLink = [
+    {
+      path: '/home',
+      title: 'Home',
+    },
+    {
+      path: '/movie',
+      title: 'Movie',
+      children: [
+        {
+          path: '/movie',
+          title: 'Movie List',
+        },
+        {
+          path: '/movie/create',
+          title: 'Create Movie',
+        },
+      ],
+    },
+  ];
   return (
     <header className="fixed top-0 left-0 w-full pb-3 pt-3 bg-gradient-to-b	from-black dark z-50">
       <nav className="container">
@@ -58,74 +78,42 @@ export default function AppHeader() {
             <NavigationMenuItem>
               <h1 className="text-3xl font-bold">Owl movie</h1>
             </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger className="bg-transparent">
-                Getting started
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                  <li className="row-span-3">
-                    <NavigationMenuLink asChild>
-                      <a
-                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                        href="/"
+            {headerLink.map((link) => {
+              return (
+                <NavigationMenuItem key={link.title}>
+                  {link.children?.length ? (
+                    <>
+                      <NavigationMenuTrigger className="bg-transparent">
+                        {link.title}
+                      </NavigationMenuTrigger>
+                      <NavigationMenuContent>
+                        <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                          {link.children?.map((component) => (
+                            <ListItem
+                              key={component.title}
+                              href={component.path}
+                            >
+                              {component.title}
+                            </ListItem>
+                          ))}
+                        </ul>
+                      </NavigationMenuContent>
+                    </>
+                  ) : (
+                    <Link href={link.path} legacyBehavior passHref>
+                      <NavigationMenuLink
+                        className={[
+                          navigationMenuTriggerStyle(),
+                          'bg-transparent ',
+                        ].join(' ')}
                       >
-                        <div className="mb-2 mt-4 text-lg font-medium">
-                          shadcn/ui
-                        </div>
-                        <p className="text-sm leading-tight text-muted-foreground">
-                          Beautifully designed components that you can copy and
-                          paste into your apps. Accessible. Customizable. Open
-                          Source.
-                        </p>
-                      </a>
-                    </NavigationMenuLink>
-                  </li>
-                  <ListItem href="/docs" title="Introduction">
-                    Re-usable components built using Radix UI and Tailwind CSS.
-                  </ListItem>
-                  <ListItem href="/docs/installation" title="Installation">
-                    How to install dependencies and structure your app.
-                  </ListItem>
-                  <ListItem
-                    href="/docs/primitives/typography"
-                    title="Typography"
-                  >
-                    Styles for headings, paragraphs, lists...etc
-                  </ListItem>
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger className="bg-transparent">
-                Components
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                  {components.map((component) => (
-                    <ListItem
-                      key={component.title}
-                      title={component.title}
-                      href={component.href}
-                    >
-                      {component.description}
-                    </ListItem>
-                  ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/docs" legacyBehavior passHref>
-                <NavigationMenuLink
-                  className={[
-                    navigationMenuTriggerStyle(),
-                    'bg-transparent',
-                  ].join(' ')}
-                >
-                  Documentation
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
+                        {link.title}
+                      </NavigationMenuLink>
+                    </Link>
+                  )}
+                </NavigationMenuItem>
+              );
+            })}
           </NavigationMenuList>
         </NavigationMenu>
       </nav>
